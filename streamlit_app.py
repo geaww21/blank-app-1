2227613be2a3db4538f211dfafe64bc4e80cbe02
geaww21 <geariffaajaelani21@gmail.com>
2025-06-03 20:46:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selamat Datang Di Web Informatika")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+"ngodingseru bersama Bapak Hendri Setiadi"
 )
+st.image(https://github.com/geaww21/blank-app-1/width=200
